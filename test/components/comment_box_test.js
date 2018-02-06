48d0cde92191be90e662a199086c4ce1576b4576
Zali Perlow <zali.perlow@flatironschool.com>
2018-02-05 20:11:27 -0500

    write test for and implement class

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,6 +3,11 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
 
+  it('has the correct class', () => {
+    const component = renderComponent(CommentBox);
+    expect(component).to.have.class('comment-box');
+  });
+
   it('has a text area', () => {
     const component = renderComponent(CommentBox)
     expect(component).to.exist;
